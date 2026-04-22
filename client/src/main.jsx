464b24bd8dc3f27a192e508bbcfc5bbd6da8ab19
Rajat072005 <trehanrajat4@gmail.com>
2026-04-22 15:25:41 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { store } from './app/store.js'
 import { Provider } from 'react-redux'
+import { LoaderProvider } from "./context/LoaderContext";
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <LoaderProvider>
+        <App />
+     </LoaderProvider>
     </Provider>
   </StrictMode>,
 )
