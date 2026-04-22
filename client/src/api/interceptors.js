c24a11cb4b6cf8fe60ffab1c410c8fceaae50ae2
Rajat072005 <trehanrajat4@gmail.com>
2026-04-22 15:20:12 +0530

    Update interceptors.js

--- client/src/api/interceptors.js
+++ client/src/api/interceptors.js
@@ -1,8 +1,11 @@
 import { api } from "./api";
-
+let isSet = false;
 let requestCount = 0;
 
 export const setupInterceptors = (setLoading) => {
+  if (isSet) return;
+  isSet = true;
+  
   api.interceptors.request.use((config) => {
     requestCount++;
     setLoading(true);
